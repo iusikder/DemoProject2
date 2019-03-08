7ed0124211ce3262611ca6be869a0fd1a12878f9
iusikder <iusikder@yahoo.com>
2019-03-08 04:29:53 -0500

    My Second Commit
    Change-Id: Ib665b27052ff68bdacde8bab6e3896c9ac487e6d
    Signed-off-by: iusikder <iusikder@yahoo.com>

---
+++ DemoProject2/src/com/demp/project/LoginPageTest.java
@@ -0,0 +1,10 @@
+package com.demp.project;
+
+import org.testng.annotations.Test;
+
+public class LoginPageTest {
+	@Test
+	public void loginTest(){
+		System.out.println("Login Test Successful");
+	}
+}
