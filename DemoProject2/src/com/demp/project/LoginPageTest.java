19611462a2d34129eec7f668b21c229c5ccc1baa
iusikder <iusikder@yahoo.com>
2019-03-08 04:54:47 -0500

    Another commit

--- DemoProject2/src/com/demp/project/LoginPageTest.java
+++ DemoProject2/src/com/demp/project/LoginPageTest.java
@@ -1,10 +1,19 @@
 package com.demp.project;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class LoginPageTest {
+	
+	WebDriver driver;
 	@Test
 	public void loginTest(){
+
+        System.setProperty("webdriver.gecko.driver", "C:\\AllDrivers\\NewGeckoDriver\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.get("https://www.freecrm.com/");
+
 		System.out.println("Login Test Successful !!!!!");
 		
 	}
